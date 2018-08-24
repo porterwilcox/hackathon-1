92edeeebf0863d8fff7fcb8039f13469a028412a
Isaiah White <isaiahwhite208@gmail.com>
2018-08-24 13:14:29 -0600

    push

---
+++ www/app/main.js
@@ -0,0 +1,15 @@
+import UserController from "./components/user/user-controller.js";
+import PostController from "./components/post/post-controller.js";
+import CommentController from "./components/comment/comment-controller.js";
+
+class App {
+ constructor() {
+  this.controllers = {
+   user: new UserController,
+   post: new PostController,
+   comment: new CommentController
+  }
+ }
+}
+
+window.app = new App
--- www/app/models/post.js
+++ www/app/models/post.js
@@ -1,27 +1,27 @@
 export default class Post {
- constructor(data) {
-  this._id = data._id
-  this.description = data.description
-  this.userId = data.userId
- }
+  constructor(data) {
+    this._id = data._id
+    this.description = data.description
+    this.userId = data.userId
+  }
 
- get listTemplate() {
-  return `
+  get listTemplate() {
+    return `
   <div class="post">
     ${this.description}
   </div>
   `
- }
+  }
 
- get detailsTemplate() {
-  return `
+  get detailsTemplate() {
+    return `
   <div>
     <h1>
       ${this.description}
     </h1>
-    <div id="activities"></div>
+    <div id="post"></div>
   </div>
   `
- }
+  }
 
 }
