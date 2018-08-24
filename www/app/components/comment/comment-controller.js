be873f5a2403ae5a630eb73217314a8f18eb6181
Isaiah White <isaiahwhite208@gmail.com>
2018-08-24 14:53:00 -0600

    comment test

--- www/app/components/comment/comment-controller.js
+++ www/app/components/comment/comment-controller.js
@@ -1,3 +1,15 @@
-export default class CommentController {
+import Store from "../../store/store.js";
 
+let store = new Store()
+
+function draw() {
+ let template = ''
+ store.state.comments.forEach(comment => template += comment.listTemplate)
+ document.getElementById('comments').innerHTML = template
+}
+
+export default class CommentController {
+ getComments() {
+  store.getComments(draw)
+ }
 }
