e7d7eb6cab9d4a0ecb9412898d4165a6275fe347
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-08-24 10:27:04 -0600

    models

---
+++ server/models/comment.js
@@ -0,0 +1,31 @@
+let mongoose = require('mongoose')
+let Schema = mongoose.Schema
+let schemaName = 'Comment'
+let ObjectId = Schema.Types.ObjectId
+
+let schema = new Schema({
+    userId: {
+        type: ObjectId,
+        ref: "User",
+        required: true
+    },
+    postId: {
+        type: ObjectId,
+        ref: "Post",
+        required: true
+    },
+    timestamp: {
+        type: Number,
+        required: true
+    },
+    voteCount: {
+        type: Number,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model(schemaName, schema)
