81f32c7251d16229909c48b9441b228ba03dd313
Isaiah White <isaiahwhite208@gmail.com>
2018-08-24 13:47:35 -0600

    Minor fixes

--- www/app/components/post/post-controller.js
+++ www/app/components/post/post-controller.js
@@ -1,3 +1,16 @@
-export default class PostController {
+import Store from "../../store/store.js";
+
+let store = new Store()
+
+function draw() {
+ let template = ''
+ store.state.posts.forEach(post => template += post.listTemplate)
+ document.getElementById('posts').innerHTML = template
+}
+
+export default class PostConroller {
+ getPosts() {
+  store.getPosts(draw)
+ }
 
 }
