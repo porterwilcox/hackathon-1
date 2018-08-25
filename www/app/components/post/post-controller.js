182991cfc76730ef48f3146cfcbfe40b8b55921d
Christopher Carlson <c.m.carlson13@gmail.com>
2018-08-24 18:50:27 -0600

    modal changes

--- www/app/components/post/post-controller.js
+++ www/app/components/post/post-controller.js
@@ -3,18 +3,24 @@ import Store from "../../store/store.js";
 let store = new Store()
 
 function draw() {
- let template = ''
- store.state.posts.forEach(post => template += post.listTemplate)
- document.getElementById('posts').innerHTML = template
+  let template = ''
+  store.state.posts.forEach(post => template += post.listTemplate)
+  document.getElementById('posts').innerHTML = template
 }
 
 export default class PostConroller {
- getPosts() {
-  store.getPosts(draw)
- }
+  getPosts() {
+    store.getPosts(draw)
+  }
 
- drawPost() {
-  let post = store.state.post
-  document.querySelector('.post-modal').style.display = 'block'
- }
+  drawPost() {
+    let post = store.state.post
+    document.querySelector('.post-modal').style.display = 'block'
+  }
+
+  window.onclick = function (event) {
+    if (event.target == document.querySelector('.post-modal')) {
+      document.querySelector('.post-modal').style.display = 'none'
+    }
+  }
 }
